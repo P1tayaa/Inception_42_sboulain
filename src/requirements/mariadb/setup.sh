0eae6bf6638eee624a8a404ae7e799af21d2b58b
P1tayaa <samuel.boulain@yahoo.fr>
2024-10-30 10:22:35 -0400

    it works

--- src/requirements/mariadb/setup.sh
+++ src/requirements/mariadb/setup.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-service mariadb start 
+service mariadb start
 
-mysql_secure_installation << _EOF_
-n
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-y
-n
-y
-y
-_EOF_
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE_NAME}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE_NAME}\`.* TO \`${MYSQL_USER}\`@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$WP_ADMIN_PASSWORD' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-cat db1.sql
-
-# mariadb -u root -p$MYSQL_ROOT_PASSWORD < db1.sql
-# rm db1.sql
-# mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-# mariadb
-
-mysql < db1.sql
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld
+exec mysqld_safe
--- src/requirements/wordpress/setup.sh
+++ src/requirements/wordpress/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+ping_mariadb_container() {
+  nc -zv mariadb 3306 >/dev/null # ping the mariadb container
+  return $?                      # return the exit status of the ping command
+}
 
 mkdir /var/www
 mkdir /var/www/html
 
 rm -rf /var/www/html/*
-mv wp-config.php /var/www/html/wp-config.php
+# mv wp-config.php /var/www/html/wp-config.php
 cd /var/www/html
-
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
@@ -15,38 +18,54 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --allow-root
-
 # mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-sed -i "s/MYSQL_DATABASE_NAME/$MYSQL_DATABASE_NAME/1" wp-config.php
-sed -i "s/MYSQL_USER/$MYSQL_USER/1" wp-config.php
-sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/1" wp-config.php
+while [ 0 -eq 0 ]; do
+  ping_mariadb_container
+  if [ $? -eq 0 ]; then
+    echo "mariadb is online"
+    break
+  else
+    echo "waiting..."
+    sleep 1
+  fi
+done
+
+# sed -i "s/MYSQL_DATABASE_NAME/$MYSQL_DATABASE_NAME/1" wp-config.php
+# sed -i "s/MYSQL_USER/$MYSQL_USER/1" wp-config.php
+# sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/1" wp-config.php
 
 # pwd
 # sleep 5
 # wp config create --dbname=$MYSQL_DATABASE_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
 # sleep 5
 
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+  echo "making wp-config"
+  wp core download --path="/var/www/html" --allow-root
+  echo "[CREATING WP CONFIG]"
+  wp core config --path="/var/www/html" --dbhost=mariadb:3306 --dbname="$MYSQL_DATABASE_NAME" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+  echo "[CREATING WP CORE]"
+  wp core install --path="/var/www/html" --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+  echo "[CREATING WP USER]"
+  wp user create --path="/var/www/html" $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
+else
+  echo "already-wp-config"
+fi
 cat wp-config.php
 
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+# wp theme install astra --activate --allow-root
 
-wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
-
-
-wp theme install astra --activate --allow-root
-
-wp plugin update --all --allow-root
+# wp plugin update --all --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/*/fpm/pool.d/www.conf
 
 # cat /etc/php/*/fpm/pool.d/www.conf | grep listen
 
-mkdir /run/php
+mkdir -p /run/php
 
-wp redis enable --allow-root
+# wp redis enable --allow-root
 
-service php7.4-fpm stop
+# service php7.4-fpm stop
 
-/usr/sbin/php-fpm7.4 -F
+exec /usr/sbin/php-fpm7.4 -F
